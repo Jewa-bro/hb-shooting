ad006762f90a5e9aba6e4c40a04436944ca526ec
Jewa-bro <wlsdyd103@gmail.com>
2025-05-19 01:00:59 +0900

    Revert "``"
    This reverts commit 175e9b915210221640bd5623707a3602c5c486fb.

--- migrate.js
+++ migrate.js
@@ -1,39 +1,5 @@
 const admin = require('firebase-admin');
-require('dotenv').config();
-
-// 필수 환경 변수 검증
-const requiredEnvVars = [
-  'FIREBASE_TYPE',
-  'FIREBASE_PROJECT_ID',
-  'FIREBASE_PRIVATE_KEY_ID',
-  'FIREBASE_PRIVATE_KEY',
-  'FIREBASE_CLIENT_EMAIL',
-  'FIREBASE_CLIENT_ID',
-  'FIREBASE_AUTH_URI',
-  'FIREBASE_TOKEN_URI',
-  'FIREBASE_AUTH_PROVIDER_CERT_URL',
-  'FIREBASE_CLIENT_CERT_URL'
-];
-
-const missingEnvVars = requiredEnvVars.filter(varName => !process.env[varName]);
-if (missingEnvVars.length > 0) {
-  console.error('다음 환경 변수가 설정되지 않았습니다:', missingEnvVars.join(', '));
-  console.error('.env 파일이 프로젝트 루트 디렉토리에 있는지 확인해주세요.');
-  process.exit(1);
-}
-
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-};
+const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
